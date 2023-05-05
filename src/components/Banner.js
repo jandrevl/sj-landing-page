071c968cb2694aa75d6ea3e69960272a5ea52bdf
Joao Lopes <jandrevl@gmail.com>
2023-05-06 00:14:07 +0100

    working on the Banner component

---
+++ src/components/Banner.js
@@ -0,0 +1,18 @@
+import './Banner.css';
+
+const Banner = () => {
+    return ( 
+        <div className='Banner'>
+            <div className="banner-image">
+                <div className="banner-text">
+                    <h1>Sandra Joaquim</h1>
+                    <h2>Serviços de Solicitadoria</h2>
+                    <h3>Olhão - Algarve</h3>
+                </div>
+            </div>
+
+        </div>
+     );
+}
+ 
+export default Banner;
