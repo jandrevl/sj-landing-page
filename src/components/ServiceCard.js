41339f18714156054ea8a93fe39c4e33ba3e00f1
Joao Lopes <jandrevl@gmail.com>
2023-05-12 22:10:22 +0100

    Card component created. Now to implement a for loop in Services component

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -5,6 +5,12 @@ const ServiceCard = () => {
     return ( 
     <>
     <div className="ServiceCard">
+        <div className="image">
+        <img src="/images/consultoria.png" alt="Heranças/Sucessões" />
+        </div>
+        <div className="service-description">
+        <p>Consultoria Jurídica</p>
+        </div>
 
     </div>
     
